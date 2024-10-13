a659297f4f5ef491f89366f656334e09f3ba6a32
NOTHINGx555 <170884425+NOTHINGx555@users.noreply.github.com>
2024-10-13 12:25:06 +0200

    Update NOTHING-AUTO-GOL.lua

--- NOTHING-AUTO-GOL.lua
+++ NOTHING-AUTO-GOL.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/6561d47efd12616cbb8314386abade89/raw/961929337bf56fd1545243d1e617a14bc5e8e0be/.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/6561d47efd12616cbb8314386abade89/raw/9a55d7ea51ad6ace79801e485519d62e963435ef/.lua"))()
